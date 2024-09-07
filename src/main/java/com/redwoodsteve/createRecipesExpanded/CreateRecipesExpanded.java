bd7ae702be2a5b71ce3c2f460baeb817b79db7e6
redwoodsteve <chrislanders700@gmail.com>
2024-09-07 11:11:28 -0700

    hello world

---
+++ src/main/java/com/redwoodsteve/createRecipesExpanded/CreateRecipesExpanded.java
@@ -0,0 +1,11 @@
+package com.redwoodsteve.createRecipesExpanded;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CreateRecipesExpanded.MODID)
+public class CreateRecipesExpanded {
+    
+    public static final String MODID = "create_recipes_expanded";
+    
+}
